91288b87d688684949a9ce6da159615cf09e4b88
Johnny Wong <johnny.wong2324@gmail.com>
2022-09-03 14:53:41 -0400

    start lab1

---
+++ labs/1/app.js
@@ -0,0 +1,19 @@
+// Name: Johnny Wong
+// Course: CS-554-WS
+// Date: September 8, 2022
+// Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+
+const express = require('express')
+const app = express()
+const configRoutes = require('./routes')
+
+app.use(express.json())
+configRoutes(app)
+
+// middleware
+
+
+app.listen(3000, () => {
+    console.log('We\'ve now got a server!')
+    console.log('Your routes will be running on http://localhost:3000')
+})
---
+++ labs/1/routes/index.js
@@ -0,0 +1,16 @@
+// Name: Johnny Wong
+// Course: CS-554-WS
+// Date: September 8, 2022
+// Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+
+const constructorMethod = app => {
+
+    // app.use('/bands', bandsRouter);
+    // app.use('/albums', albumsRouter);
+
+    app.use('*', (req, res) => {
+        res.status(404).json({ error: 'Route not found' })
+    });
+};
+
+module.exports = constructorMethod
