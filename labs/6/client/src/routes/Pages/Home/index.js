b2f6fa6ad8e1448eee5f31ce798cff980f1a0d97
Johnny Wong <johnny.wong2324@gmail.com>
2022-11-11 15:57:53 -0500

    home

---
+++ labs/6/client/src/routes/Pages/Home/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <Stack direction="vertical" gap={3} className='mx-auto py-4'>
+            <p className='h5 mx-auto'>
+                This Pokemon Lab utilizes React, Redux, Redis, Express, and GraphQL to display information from
+                the <a href='https://pokeapi.co/'>pokeapi</a> and create trainers to catch them.
+            </p>
+            <Button variant="info" as={Link} to='/trainers' className='w-25 mx-auto'>Get Started</Button>
+            <Button variant="secondary" as={Link} to='/pokemon' className='w-25 mx-auto'>View Pokemon</Button>
+        </Stack>
+    )
+}
+
+export default Home;
